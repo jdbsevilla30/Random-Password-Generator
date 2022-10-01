755051d366c50b2e6899cb3e0ebbe4b2d9c31465
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-10-01 13:17:35 +0800

    v1
    Added HTML boilerplate
    Added 1 password generator for JS

---
+++ index.js
@@ -0,0 +1,17 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+let passwordStorage = "";
+for (let i = 0; i <= 15; i++) {
+
+
+
+    passwordStorage += characters[Math.floor(Math.random() * characters.length)];
+
+
+
+    if (passwordStorage.length === 15) {
+        console.log(passwordStorage);
+    }
+
+}
