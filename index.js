087d4babf3878da70f5b75cde0d7552732b68915
John Dave B. Sevilla <98524643+jdbsevilla30@users.noreply.github.com>
2023-01-11 20:26:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,131 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
-
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
 let firstPassword = document.getElementById("first-password");
 let secondPassword = document.getElementById("second-password");
-
-
-
 
 let passLength = 15;
 
 let passwordStorageFirst = "";
 let passwordStorageSecond = "";
 
-
 function generate() {
-    for (let i = 0; i <= passLength; i++) {
-        passwordStorageFirst += generateChar1();
-        passwordStorageSecond += generateChar2();
-        if (passwordStorageFirst.length === passLength && passwordStorageSecond.length === passLength) {
-            console.log(passwordStorageFirst);
-            console.log(passwordStorageSecond);
-            firstPassword.textContent = passwordStorageFirst;
-            secondPassword.textContent = passwordStorageSecond;
-            passwordStorageFirst = "";
-            passwordStorageSecond = "";
-        }
+  for (let i = 0; i <= passLength; i++) {
+    passwordStorageFirst += generateChar1();
+    passwordStorageSecond += generateChar2();
+    if (
+      passwordStorageFirst.length === passLength &&
+      passwordStorageSecond.length === passLength
+    ) {
+      console.log(passwordStorageFirst);
+      console.log(passwordStorageSecond);
+      firstPassword.textContent = passwordStorageFirst;
+      secondPassword.textContent = passwordStorageSecond;
+      passwordStorageFirst = "";
+      passwordStorageSecond = "";
     }
+  }
 }
-
-
 
 function generateChar1() {
-    let randomChar1 = Math.floor(Math.random() * characters.length);
-    return characters[randomChar1];
-
+  let randomChar1 = Math.floor(Math.random() * characters.length);
+  return characters[randomChar1];
 }
-
 
 function generateChar2() {
-    let randomChar2 = Math.floor(Math.random() * characters.length);
-    return characters[randomChar2];
+  let randomChar2 = Math.floor(Math.random() * characters.length);
+  return characters[randomChar2];
 }
 
+function three() {}
